5def59b0eddc7e6c5d8fc6cae1678bac776702db
veetihaaja <veeti.haaja1@gmail.com>
2026-09-11 12:02:25 +0300

    recooked dynamic dt calculations and checking, tested and seems to work. Cleaned up implementation, removed debug prints, changed it so restarts happen on fractimestep 0

--- timeclasses.hpp
+++ timeclasses.hpp
@@ -36,14 +36,16 @@ std::vector<CellID> checkCellTimeclasses(dccrg::Dccrg<SpatialCell,dccrg::Cartesi
 
 void updateTimeclassDts(Real fsdt, const bool applyModifier = true);
 
-void increaseTimeclass(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid,
-                              const std::vector<CellID>& cellsToIncreaseTimeclass,
-                              bool& additionalTimeclassCreated);
+// void increaseTimeclass(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid,
+//                               const std::vector<CellID>& cellsToIncreaseTimeclass,
+//                               bool& additionalTimeclassCreated);
 
 void calculateGlobalTcVariables(Real fsdt, Real globalMaxDt);
 
 void initiateAllCellTimeclasses(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid);
 
 void timeclassDebugAssertions(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid);
+
+Real getNewSmallestDtToKeepTimeclassesHappy(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid, const std::vector<CellID> badCells);
 
 #endif
